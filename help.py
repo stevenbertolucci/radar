2da5beb3407bb607d121de8c8872cc67bd24e906
stevenbertolucci <steven.bertolucci@yahoo.com>
2024-03-01 11:23:40 -0800

    Implemented help menu messages

--- help.py
+++ help.py
@@ -1,5 +1,29 @@
-def help_menu(choice):
+def help_menu(pos):
 
-    if choice == "-h" or choice == "--help" or choice == "-help":
+    if pos == 'default':
         print("Help Menu Coming Soon!")
+        input("Press any key to continue... ")
+
+    if pos == 'weather':
+        print("Weather Help Menu Coming Soon!")
+        input("Press any key to continue... ")
+
+    if pos == 'news':
+        print("News Help Menu Coming Soon!")
+        input("Press any key to continue... ")
+
+    if pos == 'time':
+        print("Time Help Menu Coming Soon!")
+        input("Press any key to continue... ")
+
+    if pos == 'password':
+        print("Password Help Menu Coming Soon!")
+        input("Press any key to continue... ")
+
+    if pos == 'blackjack':
+        print("Blackjack Help Menu Coming Soon!")
+        input("Press any key to continue... ")
+
+    if pos == 'rickroll':
+        print("FBI Help Menu Coming Soon!")
         input("Press any key to continue... ")
